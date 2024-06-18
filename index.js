2bee3581c6dd90fcec9d9c5497b129b36306e136
Marlon Munoz <marlon@marlonshade.com>
2024-06-18 05:56:57 -0400

    array lab completed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   return cats.push("Ralph"); 
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = cats.slice();
+    newCats.push("Broom");
+    return newCats;
+}
+
+function prependCat(name) {
+    const myCats = cats.slice();
+    myCats.unshift("Arnold");
+    return myCats;
+}
+
+function removeLastCat() {
+    const newAnimals = cats.slice();
+    newAnimals.pop()
+    return newAnimals;
+}
+
+function removeFirstCat() {
+    const pets = cats.slice();
+    pets.shift();
+    return pets;    
+}
